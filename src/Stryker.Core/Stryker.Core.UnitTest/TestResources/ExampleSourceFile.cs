8046107afa75d55e4ce86fd94ca9b5225dd65700
Guthias <gmgustavom@gmail.com>
2023-06-17 22:03:04 -0300

    adjustin namespace for Stryker.Core.UnitTest

--- src/Stryker.Core/Stryker.Core.UnitTest/TestResources/ExampleSourceFile.cs
+++ src/Stryker.Core/Stryker.Core.UnitTest/TestResources/ExampleSourceFile.cs
@@ -1,35 +1,34 @@
 using System;
 
-namespace ExampleProject
+namespace ExampleProject;
+
+public class Recursive
 {
-    public class Recursive
+    public int Fibinacci(int len)
     {
-        public int Fibinacci(int len)
-        {
-            return Fibonacci(0, 1, 1, len);
-        }
+        return Fibonacci(0, 1, 1, len);
+    }
 
-        private int Fibonacci(int a, int b, int counter, int len)
+    private int Fibonacci(int a, int b, int counter, int len)
+    {
+        if (counter <= len)
         {
-            if (counter <= len)
-            {
-                return Fibonacci(b, a + b, counter + 1, len);
-            }
-            return 0;
+            return Fibonacci(b, a + b, counter + 1, len);
         }
+        return 0;
+    }
 
-        private string LoremIpsum()
-        {
-            return @"Lorem Ipsum
+    private string LoremIpsum()
+    {
+        return @"Lorem Ipsum
                     Dolor Sit Amet
                     Lorem Dolor Sit";
-        }
+    }
 
-        private void StringSplit()
-        {
-            var testString = "";
+    private void StringSplit()
+    {
+        var testString = "";
 
-            testString.Split("\n");
-        }
+        testString.Split("\n");
     }
 }
